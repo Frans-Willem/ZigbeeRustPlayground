3593c2b8839de2e94d3a44423bbd7a045352154a
Frans-Willem Hardijzer <fw@hardijzer.nl>
2019-04-16 20:25:08 +0200

    Switch over to non-raw service.

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,33 @@ extern crate tokio_sync;
 
 mod radio_bridge;
 
-use bytes::{BufMut, BytesMut};
+use std::sync::Arc;
 use tokio::prelude::{Future, Stream};
 use tokio_core::reactor::Core;
-use tokio_service::Service;
 
 fn main() {
     let settings = tokio_serial::SerialPortSettings::default();
     let port = tokio_serial::Serial::from_path("/dev/ttyACM0", &settings).unwrap();
     let mut core = Core::new().unwrap();
 
-    let (service, packet_stream) = radio_bridge::raw_service::RadioBridgeService::new(port, core.handle());
+    let (service, packet_stream) =
+        radio_bridge::service::RadioBridgeService::new(port, core.handle());
 
-    core.handle().spawn(packet_stream.for_each(|pkt| {
-        println!("Got a packet of length {}", pkt.packet.len());
-        Ok(())
-    }).map_err(|e| eprintln!("{:?}", e)));
+    let packet_handler = packet_stream
+        .for_each(|pkt| {
+            println!("Got a packet of length {}", pkt.packet.len());
+            Ok(())
+        })
+        .map_err(|e| eprintln!("{:?}", e));
+    let service = Arc::new(service);
 
-    let mut data = BytesMut::new();
-    data.put_u16_be(11);
-    data.put_u16_be(8);
-    let response = service.call(radio_bridge::raw_service::Request {
-    command_id: radio_bridge::raw_service::RequestCommand::RadioGetObject,
-    data: data.freeze(),
-    });
-    let response = core.run(response).unwrap();
-    println!("Got ID {:?}", response);
+    let setup_response = service
+        .set_channel(11)
+        .join(service.set_rx_mode(0))
+        .join(service.on())
+        .and_then(|_| Ok(println!("Setup complete")))
+        .map_err(|e| eprintln!("{:?}", e));
+    core.handle().spawn(setup_response);
+
+    core.run(packet_handler).unwrap();
 }
