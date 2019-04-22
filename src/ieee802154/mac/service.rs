062a855c3963b15bf1cfa3f565bc9069ef4e55a9
Frans-Willem Hardijzer <fw@hardijzer.nl>
2019-04-22 16:42:27 +0200

    More work on MAC Layer service. Will expand later.

--- src/ieee802154/mac/service.rs
+++ src/ieee802154/mac/service.rs
@@ -1,20 +1,32 @@
 use crate::ieee802154::mac::frame::*;
 use crate::ieee802154::{ExtendedAddress, ShortAddress, PANID};
-use bytes::Bytes;
+use crate::parse_serialize::{ParseFromBuf, SerializeToBuf};
+use bytes::{Bytes, IntoBuf};
 use futures::future::Future as _;
+use futures::stream::Stream as _;
+use futures::sync::mpsc;
+use std::rc::Rc;
 use std::sync::Mutex;
 use tokio_core::reactor::Handle;
 
+use crate::parse_serialize::Error as ParseError;
 use crate::radio_bridge::service::Error as RadioError;
 use crate::radio_bridge::service::IncomingPacket as RadioPacket;
 use crate::radio_bridge::service::RadioBridgeService as RadioService;
 use crate::radio_bridge::service::RadioRxMode;
 
+struct InnerService {
+    handle: Handle,
+    radio: RadioService,
+    event_sink: mpsc::UnboundedSender<Event>,
+    sequence_number: Mutex<u8>,
+}
+
 pub struct Service {
+    inner: Rc<InnerService>,
     pan_id: PANID,
     short_address: ShortAddress,
     extended_address: ExtendedAddress,
-    sequence_number: Mutex<u8>,
 }
 
 pub enum Event {
@@ -24,12 +36,77 @@ pub enum Event {
 #[derive(Debug)]
 pub enum Error {
     RadioError(RadioError),
+    MPSCError,
+    SerializationError(ParseError),
+}
+impl From<RadioError> for Error {
+    fn from(err: RadioError) -> Self {
+        Error::RadioError(err)
+    }
 }
 
 type Future<T> = futures::Future<Item = T, Error = Error>;
 type BoxFuture<T> = Box<Future<T>>;
 type Stream<T> = futures::Stream<Item = T, Error = Error>;
 type BoxStream<T> = Box<Stream<T>>;
+
+impl InnerService {
+    fn new(handle: &Handle, radio: RadioService) -> (InnerService, mpsc::UnboundedReceiver<Event>) {
+        let (sender, receiver) = mpsc::unbounded();
+        (
+            InnerService {
+                handle: handle.clone(),
+                radio,
+                event_sink: sender,
+                sequence_number: Mutex::new(0),
+            },
+            receiver,
+        )
+    }
+    fn push_event(&self, event: Event) {
+        self.event_sink.unbounded_send(event).unwrap()
+    }
+
+    fn fresh_sequence_number(&self) -> u8 {
+        let mut guard = self.sequence_number.lock().unwrap();
+        let retval = *guard;
+        *guard = *guard + 1;
+        retval
+    }
+
+    fn on_incoming_frame(&self, frame: Frame, _rssi: u8, _link_quality: u8) {
+        println!("<< {:?}", frame);
+        match frame.frame_type {
+            FrameType::Command(Command::BeaconRequest) => {
+                println!("Got a beacon requets :)");
+                self.push_event(Event::BeaconRequest())
+            }
+            _ => println!("Unhandled"),
+        }
+    }
+
+    fn on_incoming_packet(&self, packet: RadioPacket) {
+        match Frame::parse_from_buf(&mut packet.packet.clone().into_buf()) {
+            Err(e) => eprintln!("Unable to parse MAC packet, {:?}: {:?}", e, packet.packet),
+            Ok(frame) => self.on_incoming_frame(frame, packet.rssi, packet.link_quality),
+        }
+    }
+
+    fn send_raw_frame(&self, frame: Frame) -> BoxFuture<()> {
+        // TODO: This should only return if both the send succeeds, and (if required) an Ack was
+        // received.
+        let mut serialized = vec![];
+        if let Err(e) = frame.serialize_to_buf(&mut serialized) {
+            Box::new(futures::future::err(Error::SerializationError(e)))
+        } else {
+            Box::new(
+                self.radio
+                    .send(serialized.into())
+                    .map_err(Error::RadioError),
+            )
+        }
+    }
+}
 
 impl Service {
     pub fn new(
@@ -39,9 +116,9 @@ impl Service {
         channel: u16,
         short_address: ShortAddress,
         pan_id: PANID,
-    ) -> BoxFuture<(Service, Stream<Event>)> {
+    ) -> BoxFuture<(Service, BoxStream<Event>)> {
         let max_power = radio.get_tx_power_max().map(move |pwr| (pwr, radio));
-        let set_properties = max_power.and_then(|(max_power, radio)| {
+        let set_properties = max_power.and_then(move |(max_power, radio)| {
             let f1 = radio.set_tx_power(max_power);
             let f2 = radio.set_channel(channel);
             let f3 = radio.set_short_address(short_address.as_u16());
@@ -54,14 +131,37 @@ impl Service {
             f1.join5(f2, f3, f4, f5).map(move |_| radio)
         });
         let turn_on = set_properties.and_then(|radio| radio.on().map(move |_| radio));
-        unimplemented!();
+        let extended_address = turn_on.and_then(|radio| {
+            radio
+                .get_long_address()
+                .map(move |extaddr| (extaddr, radio))
+        });
+        let service = extended_address.map(move |(extended_address, radio)| {
+            let (inner, events) = InnerService::new(&handle, radio);
+            let inner = Rc::new(inner);
+            let packet_inner = inner.clone();
+            handle.spawn(
+                packetstream
+                    .for_each(move |packet| Ok(packet_inner.on_incoming_packet(packet)))
+                    .map_err(|e| eprintln!("Radio packet error: {:?}", e)),
+            );
+            (
+                Service {
+                    inner,
+                    pan_id,
+                    short_address,
+                    extended_address: ExtendedAddress(extended_address),
+                },
+                Box::new(events.map_err(|_| Error::MPSCError)) as BoxStream<Event>,
+            )
+        });
+        let service = service.map_err(|e| Error::RadioError(e));
+        let service = Box::new(service);
+        service
     }
 
     fn fresh_sequence_number(&self) -> u8 {
-        let mut guard = self.sequence_number.lock().unwrap();
-        let retval = *guard;
-        *guard = *guard + 1;
-        retval
+        self.inner.fresh_sequence_number()
     }
 
     pub fn get_pan_id(&self) -> PANID {
@@ -72,11 +172,11 @@ impl Service {
         return self.short_address;
     }
 
-    fn send_raw_frame(&self, _frame: Frame) -> Box<Future<()>> {
-        unimplemented!();
+    fn send_raw_frame(&self, frame: Frame) -> BoxFuture<()> {
+        self.inner.send_raw_frame(frame)
     }
 
-    pub fn send_beacon(&self, payload: Bytes) -> Box<Future<()>> {
+    pub fn send_beacon(&self, payload: Bytes) -> BoxFuture<()> {
         let beacon = Frame {
             acknowledge_request: false,
             sequence_number: self.fresh_sequence_number().into(),
